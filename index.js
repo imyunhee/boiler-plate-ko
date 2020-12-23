3912bacfbdf527790d3f5d3ee2d5e999c9c9897d
yunhee <yun2_20@naver.com>
2020-12-24 00:06:55 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser') // json 형태로
+
+const config = require('./config/key')
+
+const {User} = require("./models/User")
+
+// application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://uniring:uniring@boilerplate.mct8x.mongodb.net/uniring?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected....')) // 연결 됐을 때
   .catch(err => console.log(err))                   // 연결 안 됐을 때
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World~ 반갑습니다:)'))
+
+app.post('/register', (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+    
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
